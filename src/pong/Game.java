b504c555c13f991d19663f38efe432737527a7ef
HelintonAlves <helinton-alves@hotmail.com>
2026-02-23 11:10:57 -0300

    Aplicando movimentação no player

--- src/pong/Game.java
+++ src/pong/Game.java
@@ -2,18 +2,24 @@ package pong;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
-public class Game extends Canvas implements Runnable {
+public class Game extends Canvas implements Runnable, KeyListener {
 
-    private static int WIDTH = 240;
-    private static int HEIGHT = 120;
-    private static int SCALE = 3;
-    private Player player;
+    public static int WIDTH = 240;
+    public static int HEIGHT = 160;
+    public static int SCALE = 3;
+    public BufferedImage layer = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
+    public Player player;
+
 
     public Game(){
         this.setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
-        player = new Player();
+        this.addKeyListener(this);
+        player = new Player(0, 60);
     }
 
     public static void main(String[] args) {
@@ -29,7 +35,7 @@ public class Game extends Canvas implements Runnable {
     }
 
     public void tick(){
-
+        player.tick();
     }
 
     public void render(){
@@ -38,22 +44,53 @@ public class Game extends Canvas implements Runnable {
             this.createBufferStrategy(3);
             return;
         }
-        Graphics g = bs.getDrawGraphics();
+        Graphics g = layer.getGraphics();
+        //Creando o player
+        g.setColor(Color.black);
+        g.fillRect(0,0,WIDTH,HEIGHT);
         player.render(g);
+
+        g = bs.getDrawGraphics();
+        //Creando a layer para o jogo
+        g.drawImage(layer, 0, 0,WIDTH*SCALE,HEIGHT*SCALE,null);
+
         bs.show();
     }
 
     @Override
     public void run() {
-        while (true){
+        while(true){
             tick();
             render();
             try {
-                Thread.sleep(1000/60);
+                Thread.sleep(1000 / 60);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
 
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W){
+            player.up = true;
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
+            player.down = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W){
+            player.up = false;
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
+            player.down = false;
+        }
     }
 }
--- src/pong/Player.java
+++ src/pong/Player.java
@@ -4,15 +4,26 @@ import java.awt.*;
 
 public class Player {
 
+    public boolean up, down;
+    public int x, y;
 
+    public Player(int x , int y){
+        this.x = x;
+        this.y = y;
+    }
 
     public void tick(){
+        if(up){
+            y--;
+        } else if (down) {
+            y++;
+        }
 
     }
 
     public void render(Graphics g){
         g.setColor(Color.blue);
-        g.fillRect(0, 160, 10, 40 );
+        g.fillRect(x, y, 10, 40 );
     }
 
 }
