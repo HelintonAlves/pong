c20ad548f162dc7a60b62d2ba761e7d2e422bfb4
HelintonAlves <helinton-alves@hotmail.com>
2026-02-23 10:25:46 -0300

    Renderizando o player

--- src/pong/Game.java
+++ src/pong/Game.java
@@ -2,15 +2,18 @@ package pong;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable {
 
     private static int WIDTH = 240;
     private static int HEIGHT = 120;
     private static int SCALE = 3;
+    private Player player;
 
     public Game(){
         this.setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
+        player = new Player();
     }
 
     public static void main(String[] args) {
@@ -22,11 +25,35 @@ public class Game extends Canvas implements Runnable {
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        new Thread(game).start();
     }
 
+    public void tick(){
+
+    }
+
+    public void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+        player.render(g);
+        bs.show();
+    }
 
     @Override
     public void run() {
+        while (true){
+            tick();
+            render();
+            try {
+                Thread.sleep(1000/60);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
     }
 }
---
+++ src/pong/Player.java
@@ -0,0 +1,18 @@
+package pong;
+
+import java.awt.*;
+
+public class Player {
+
+
+
+    public void tick(){
+
+    }
+
+    public void render(Graphics g){
+        g.setColor(Color.blue);
+        g.fillRect(0, 160, 10, 40 );
+    }
+
+}
